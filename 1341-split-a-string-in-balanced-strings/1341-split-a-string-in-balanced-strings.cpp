61fce6ab337216c56145b84fe43bd87231c3d58a
Nidhay Rathore <86305198+geekynidhay@users.noreply.github.com>
2023-10-12 23:57:06 +0530

    Time: 0 ms (100%), Space: 6.5 MB (38.52%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int r = 0, l = 0, ans = 0;
+
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == 'R') r++;
+            else if (s[i] == 'L') l++;
+            if (r == l){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
